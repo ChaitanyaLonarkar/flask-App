1f7aadb12c6c5fb07dbadedf896729d6463051a9
Chaitanyalonarkar <chaitanyalonarkar@gmail.com>
2024-04-27 22:37:18 +0530

    # ye run hua bt negative value de raha h

--- app.py
+++ app.py
@@ -130,34 +130,193 @@
 #     app.run(debug=True)
 
 
+# from flask import Flask, render_template, request
+# import numpy as np
+# from keras.models import load_model
+# from keras.losses import MeanSquaredError
+
+# app = Flask(__name__)
+
+# # Load the Keras model
+# def load_weather_model():
+#     global model
+#     model = load_model('model.h5')
+#     # Compile the model with the desired loss function
+#     # model.compile(optimizer='adam', loss='mean_squared_error')
+
+# load_weather_model()
+
+# # Function to preprocess user inputs
+# def preprocess_input(temp_max, temp_min, precipitation, humidity, pressure, wind_speed):
+#     # Convert inputs to numpy array
+#     inputs = np.array([[temp_max, temp_min, precipitation, humidity, pressure, wind_speed]])
+#     return inputs
+
+# # Function to make weather prediction
+# def predict_weather(inputs):
+#     model.compile(optimizer='adam', loss=MeanSquaredError())
+
+#     prediction = model.predict(inputs)
+#     return prediction[0][0]  # Assuming single output for weather prediction
+
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
+
+# @app.route('/predict', methods=['POST'])
+# def predict():
+#     if request.method == 'POST':
+#         # Get user inputs from the form
+#         temp_max = float(request.form['temp_max'])
+#         temp_min = float(request.form['temp_min'])
+#         precipitation = float(request.form['precipitation'])
+#         humidity = float(request.form['humidity'])
+#         pressure = float(request.form['pressure'])
+#         wind_speed = float(request.form['wind_speed'])
+
+#         # Preprocess inputs
+#         inputs = preprocess_input(temp_max, temp_min, precipitation, humidity, pressure, wind_speed)
+
+#         # Make prediction
+#         prediction = predict_weather(inputs)
+
+#         return render_template('result.html', prediction=prediction)
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+
+# from flask import Flask, render_template, request
+# import numpy as np
+# from keras.models import load_model
+# from keras.losses import MeanSquaredError
+
+# app = Flask(__name__)
+
+# # Load the Keras model
+# model = load_model('model.h5',custom_objects=None, compile=True)
+
+# # Function to preprocess user inputs
+# def preprocess_input(temp_max, temp_min, precipitation, humidity, pressure, wind_speed):
+#     # Convert inputs to numpy array
+#     inputs = np.array([[temp_max, temp_min, precipitation, humidity, pressure, wind_speed]])
+#     return inputs
+
+# # Function to make weather prediction
+# def predict_weather(inputs):
+#     # Compile model with MeanSquaredError loss function
+#     model.compile(optimizer='adam', loss=MeanSquaredError())
+#     prediction = model.predict(inputs)
+#     return prediction[0][0]  # Assuming single output for weather prediction
+
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
+
+# @app.route('/predict', methods=['POST'])
+# def predict():
+#     if request.method == 'POST':
+#         # Get user inputs from the form
+#         temp_max = float(request.form['temp_max'])
+#         temp_min = float(request.form['temp_min'])
+#         precipitation = float(request.form['precipitation'])
+#         humidity = float(request.form['humidity'])
+#         pressure = float(request.form['pressure'])
+#         wind_speed = float(request.form['wind_speed'])
+
+#         # Preprocess inputs
+#         inputs = preprocess_input(temp_max, temp_min, precipitation, humidity, pressure, wind_speed)
+
+#         # Make prediction
+#         prediction = predict_weather(inputs)
+
+#         return render_template('result.html', prediction=prediction)
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+
+# from flask import Flask, render_template, request
+# import numpy as np
+# from keras.models import load_model
+# from keras.losses import MeanSquaredError
+# # import keras
+
+# app = Flask(__name__)
+
+# # print(keras.__version__)
+
+# # Load the Keras model
+# model = load_model('model_updated.keras')
+
+# # Function to preprocess user inputs
+# def preprocess_input(temp_max, temp_min, precipitation, humidity, pressure, wind_speed):
+#     # Convert inputs to numpy array
+#     inputs = np.array([[temp_max, temp_min, precipitation, humidity, pressure, wind_speed]])
+#     return inputs
+
+# # Function to make weather prediction
+# def predict_weather(inputs):
+#     # Compile model with CustomMSE loss function
+#     model.compile(optimizer='adam', loss=MeanSquaredError)
+#     prediction = model.predict(inputs)
+#     return prediction[0][0]  # Assuming single output for weather prediction
+
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
+
+# @app.route('/predict', methods=['POST'])
+# def predict():
+#     if request.method == 'POST':
+#         # Get user inputs from the form
+#         temp_max = float(request.form['temp_max'])
+#         temp_min = float(request.form['temp_min'])
+#         precipitation = float(request.form['precipitation'])
+#         humidity = float(request.form['humidity'])
+#         pressure = float(request.form['pressure'])
+#         wind_speed = float(request.form['wind_speed'])
+
+#         # Preprocess inputs
+#         inputs = preprocess_input(temp_max, temp_min, precipitation, humidity, pressure, wind_speed)
+#         print("inputs are here",inputs)
+#         # Make prediction
+#         prediction = predict_weather(inputs)
+
+#         return render_template('result.html', prediction=prediction)
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+
 from flask import Flask, render_template, request
 import numpy as np
 from keras.models import load_model
 from keras.losses import MeanSquaredError
+import traceback
 
 app = Flask(__name__)
 
-# Load the Keras model
-def load_weather_model():
-    global model
-    model = load_model('./model.h5')
-    # Compile the model with the desired loss function
-    # model.compile(optimizer='adam', loss='mean_squared_error')
+try:
+    # Load the Keras model
+    model = load_model('model_updated.keras')
 
-load_weather_model()
+    # Function to preprocess user inputs
+    def preprocess_input(temp_max, temp_min, precipitation, humidity, pressure, wind_speed):
+        # Convert inputs to numpy array
+        inputs = np.array([[temp_max, temp_min, precipitation, humidity, pressure, wind_speed]])
+        return inputs
 
-# Function to preprocess user inputs
-def preprocess_input(temp_max, temp_min, precipitation, humidity, pressure, wind_speed):
-    # Convert inputs to numpy array
-    inputs = np.array([[temp_max, temp_min, precipitation, humidity, pressure, wind_speed]])
-    return inputs
+    # Function to make weather prediction
+    def predict_weather(inputs):
+        # Compile model with CustomMSE loss function
+        model.compile(optimizer='adam', loss=MeanSquaredError)
+        prediction = model.predict(inputs)
+        return prediction  # Assuming single output for weather prediction
 
-# Function to make weather prediction
-def predict_weather(inputs):
-    model.compile(optimizer='adam', loss=MeanSquaredError())
-
-    prediction = model.predict(inputs)
-    return prediction[0][0]  # Assuming single output for weather prediction
+except Exception as e:
+    print("An error occurred while loading the model:", e)
+    traceback.print_exc()
 
 @app.route('/')
 def index():
@@ -166,21 +325,25 @@ def index():
 @app.route('/predict', methods=['POST'])
 def predict():
     if request.method == 'POST':
-        # Get user inputs from the form
-        temp_max = float(request.form['temp_max'])
-        temp_min = float(request.form['temp_min'])
-        precipitation = float(request.form['precipitation'])
-        humidity = float(request.form['humidity'])
-        pressure = float(request.form['pressure'])
-        wind_speed = float(request.form['wind_speed'])
+        try:
+            # Get user inputs from the form
+            temp_max = float(request.form['temp_max'])
+            temp_min = float(request.form['temp_min'])
+            precipitation = float(request.form['precipitation'])
+            humidity = float(request.form['humidity'])
+            pressure = float(request.form['pressure'])
+            wind_speed = float(request.form['wind_speed'])
 
-        # Preprocess inputs
-        inputs = preprocess_input(temp_max, temp_min, precipitation, humidity, pressure, wind_speed)
+            # Preprocess inputs
+            inputs = preprocess_input(temp_max, temp_min, precipitation, humidity, pressure, wind_speed)
+            print("inputs are here",inputs)
+            # Make prediction
+            prediction = predict_weather(inputs)
 
-        # Make prediction
-        prediction = predict_weather(inputs)
-
-        return render_template('result.html', prediction=prediction)
+            return render_template('result.html', prediction=prediction)
+        except Exception as e:
+            print("An error occurred while making prediction:", e)
+            traceback.print_exc()
 
 if __name__ == '__main__':
     app.run(debug=True)
